b40f496ca2eb4b450935aa0002edf665e50ae1c3
Sung J Byun <sungjbyun@D-173-250-238-177.dhcp4.washington.edu>
2018-03-14 15:07:05 -0700

    initial: instead hardcoded data -> use real data for next

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema');
+
+
+const app = express();
+const port = process.env.PORT || 4000
+
+app.use('/graphql', expressGraphQL({
+  schema:schema,
+  graphql:true
+}));
+
+app.listen(port, (req, res) => {
+  console.log('Server running on port ', port);
+});
